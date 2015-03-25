aa94e7d1f50e90820aa6258f6fd4bb62abe8a0e8
Adam Sofokleous <adamcyp@gmail.com>
2015-03-25 19:50:01 +0000

    added font size increase and decrease functionality, organized css

--- js/inits.js
+++ js/inits.js
@@ -11,6 +11,16 @@ $(document).ready(function(){
 
 	jPM.on();
 
+	$('#incfont').click(function(){	   
+        curSize= parseInt($('#content').css('font-size')) + 2;
+		if(curSize<=20)
+        $('#content').css('font-size', curSize);
+        });  
+		$('#decfont').click(function(){	   
+        curSize= parseInt($('#content').css('font-size')) - 2;
+		if(curSize>=12)
+        $('#content').css('font-size', curSize);
+    });
 });
 
 /**
